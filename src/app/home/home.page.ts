b9c34574bb66cc16732a7570964d241560735f47
EwersonBrandina <ewerson.brandina@hotmail.com.br>
2022-10-27 10:31:04 -0300

    Atualização 10:30 - 27/10/2022

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Guid } from 'guid-typescript';
+import { Pessoa } from '../models/pessoa.model';
+import { PessoasService } from '../services/pessoas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  num1 : string
-  num2 : string
+export class HomePage implements OnInit{
+  peso : string
+  altura : string
   escolha : string
   saida : string
   saida2 : string
   img : any = "assets/icon/favicon.png"
-  constructor() {}
+  private pessoa : Pessoa
+  //public pessoaForm : FormGroup
+  //public arrayPessoa : any
+
+  constructor(
+    //private formBuilder : FormBuilder,
+    private pessoaService : PessoasService
+  ) {}
 
 
 calcular(){
-  var n1 = parseFloat(this.num1)
-  var n2 = parseFloat(this.num2)
+  var n1 = parseFloat(this.peso)
+  var n2 = parseFloat(this.altura)
   var resultado = (n1/(n2**2)).toFixed(2)
   if (this.escolha == "Homem"){
     if (Number(resultado) <= 19){
@@ -52,7 +63,34 @@ calcular(){
       this.saida2 = "Acima do Peso"
       this.img = "assets/imagem_alerta.png"
     }
-
   }
+  this.pessoa = {
+    id: Guid.createEmpty(),
+    peso : this.peso,
+    altura : this.altura,
+    saida: this.saida.toString(),
+    sexo : this.escolha,
+    saida2 : this.saida2
+  }
+  this.pessoaService.inserir(this.pessoa)
+  }
+
+  //enviar(){
+  //  if (this.pessoaForm.valid){
+  //    this.pessoaService.inserir(this.pessoaForm.value)
+  //  }
+  //}
+
+  ngOnInit(){
+  //  this.pessoa = {id: Guid.createEmpty(), peso:"", altura:"", saida:""}
+  //  this.pessoaForm = this.formBuilder.group({
+  //    id: [this.pessoa.id],
+  //    peso: [this.pessoa.peso, Validators.required],
+  //    altura: [this.pessoa.altura, Validators.required],
+  //    saida: [this.pessoa.saida, Validators.required]
+  //  })
+  //  this.pessoaService.listarTodos().then(arrayPessoa => {this.arrayPessoa = arrayPessoa})
+  }
+  
 }
-}
+
